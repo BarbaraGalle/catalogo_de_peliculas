f4c67d5990dd5f010e54308060aafe2ce7c8d27b
BarbaraGalle <galleguillo.barbarap@gmail.com>
2024-06-25 09:45:03 -0300

    Update catalogo.py

--- catalogo.py
+++ catalogo.py
@@ -36,47 +36,6 @@ class catalogo_peliculas:
         print("3.Eliminar catalogo de peliculas")
         print("4.salir")
 
-    def agregar_pelicula(self, pelicula):
-        self.peliculas.append(pelicula)
-
-    def listar_peliculas(self):
-        if not self.peliculas:
-            print("No hay películas en el catálogo.")
-        else:
-            for pelicula in self.peliculas:
-                print(f"Título: {pelicula.nombre}, Director: {pelicula.director}, Año: {pelicula.año}, Género: {pelicula.genero}")
-
-    def eliminar_catalogo(self):
-        print("Catálogo de películas eliminado.")
-
-
-    def main():
-    nombre_catalogo= input("Ingrese el nombre del catalogo de la pelicula ")
-    catalogo = catalogo_peliculas(nombre_catalogo)
-
-    while True:
-        catalogo.mostrar_menu()
-        opcion = input("Seleccionar una opcion: ")
-
-        if opcion == "1":
-          titulo = input("ingresa el titulo de la pelicula:")
-          director = input("ingresa el director de la pelicula:")
-          año = input("ingresa el año de estreno de la pelicula:")
-          genero = input("ingresa el género de la pelicula:")
-          pelicula_nueva = pelicula(titulo,director,año,genero)
-          catalogo.agregar_pelicula(pelicula_nueva)
-        elif opcion == "2":
-          catalogo.listar_peliculas()
-        elif opcion == "3":
-          catalogo.eliminar_catalogo()
-        elif opcion == "4":
-          print("Saliendo. Gracias por usar el programa")
-          break
-        else:
-          print("Opcion no valida. Por favor, intente de nuevo")
-
-
-if __name__ == "__main__":
-    main()
+ 
 
  
