f09b84d11b5ebd8a61e677a03e4b510f64053159
BarbaraGalle <galleguillo.barbarap@gmail.com>
2024-06-25 21:18:20 -0300

    Update catalogo.py

--- catalogo.py
+++ catalogo.py
@@ -2,24 +2,23 @@
 import os 
 
 class pelicula:
-    def_init_(self,nombre,genero,año,director):
-       self.nombre = nombre
-       self.genero = genero
-       self.año = año
-       self.director = director
+    def __init__(self,nombre,año,director):
+        self.nombre = nombre
+        self.año = año
+        self.director = director
     
 
 class catalogo_peliculas:
-    def _init_(self,nombre):
+    def __init__(self, nombre):
         self.nombre = nombre
         self.peliculas = []
 
-    def mostrar_menu(self):
-        print("\n--- Menu de opciones---")
-        print("1.Agregar una pelicula")
-        print("2. Mostrar peliculas")
+ def mostrar_menu(self):
+        print("\n--- Menú de opciones:---")
+        print("1. Agregar una película")
+        print("2. Mostrar películas")
         print("3.Eliminar catalogo de peliculas")
-        print("4.Salir")
+        print("4.salir")
 
     def agregar_pelicula(self, pelicula):
         self.peliculas.append(pelicula)
